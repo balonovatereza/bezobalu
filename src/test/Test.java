17a94e80e27e7426a1244734a4ba9a85030f1b68
Eva Lichnovska <ellichnovska@gmail.com>
2018-11-24 00:42:45 +0100

    upraveny parametr z idmesta na idMesta (camelcase)

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	System.out.println("ahoj ahoj"); 
 	
 	System.out.println("ahoj z nove vetve BranchEva"); 
+	
+	System.out.println("ahoj ted zkousim, proc zas mmusim prepisovat verzi javy"); 
 
 }
