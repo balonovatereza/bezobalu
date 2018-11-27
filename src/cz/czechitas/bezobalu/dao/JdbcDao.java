757d7fc3ec0bff24acab1dfcfcc2d23c83665c15
Tereza Balonova <balonova.tereza@gmail.com>
2018-11-27 13:40:05 +0100

    Uprava SELECTU v jdbcdao - razeni podle abecedy

--- src/cz/czechitas/bezobalu/dao/JdbcDao.java
+++ src/cz/czechitas/bezobalu/dao/JdbcDao.java
@@ -19,7 +19,7 @@ public class JdbcDao {
 	private static final String VYFILTRUJPRODUKTY = "SELECT p.* FROM produkt p JOIN spojovacitabulka st ON p.id = st.idproduktu WHERE st.idmesta = ? AND p.idKategorie=? ORDER BY p.nazev ASC";
 	private static final String VYFILTRUJKATEGORIE = "SELECT DISTINCT k.* FROM kategorie k JOIN produkt p ON p.idKategorie = k.id JOIN spojovacitabulka st ON p.id = st.idproduktu WHERE idmesta = ? ORDER BY k.nazev";
 	private static final String NAJDICENU = "SELECT cena FROM produkt p WHERE p.id = ?";
-	private static final String VRATMESTA = "SELECT * FROM mesto;";
+	private static final String VRATMESTA = "SELECT * FROM mesto ORDER BY nazev;";
 	private static final String VRATPRODUKT = "SELECT p.nazev FROM produkt p WHERE p.id = ?";
 	private static final String VRATKATEGORII = "SELECT k.nazev FROM kategorie k WHERE k.id = ?";
 
@@ -35,7 +35,7 @@ public class JdbcDao {
 
 	public ArrayList<Produkt> vyfiltrujProdukty(int idMesta, int idKategorie) {
 
-		ArrayList<Produkt> ret = new ArrayList<Produkt>();
+		ArrayList<Produkt> ret = new ArrayList<Produkt>(); //
 		DataSource ds = getDataSource();
 		try (Connection con = ds.getConnection(); PreparedStatement stmt = con.prepareStatement(VYFILTRUJPRODUKTY)) {
 			stmt.setInt(1, idMesta);
